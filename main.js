d505bd8e953e5c4f6b90f9f4fc9b44385850b51d
Tanisha-2010 <104196498+Tanisha-2010@users.noreply.github.com>
2023-01-17 19:57:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX = "";
+noseY = "";
+
+function preload() {
+    world_start = loadSound("world_start.wav");
+    setSprites();
+    MarioAnimation();
+}
+
+function setup() {
+    canvas = createCanvas(1240, 336);
+    instializeInSetup(mario);
+    canvas.parent("canvas");
+
+    video = createCapture(VIDEO);
+    //video.hide();
+    video.size(800, 400);
+
+    video.parent("gameConsole");
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded() {
+    console.log("Model is initialized");
+}
+
+function gotPoses(results) {
+
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+    }
+}
+
+function draw() {
+    game()
+}
